de856f06c02fafef8e76d95370556f83620de9ba
Stanislav B <bazarovstas@gmail.com>
2024-10-30 20:27:41 +0300

    add cli.py (welcome_user function), add Makefile commands, update .gitignore

--- Makefile
+++ Makefile
@@ -10,6 +10,9 @@ publish:
 package-install:
 	python -m pip install --user dist/*.whl
 
+package-reinstall:
+	pip install --upgrade --force-reinstall dist/*.whl
+
 
 brain-games:
 	poetry run brain-games
---
+++ brain_games/cli.py
@@ -0,0 +1,8 @@
+import prompt
+
+def welcome_user():
+    username = prompt.string(
+        'Welcome to the Brain Games!\n'
+        'May I have your name? '
+        )
+    print(f'Hello, {username}!')
