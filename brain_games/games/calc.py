5ec32ef4a949ede2bcf2d904578c07dc5cbd9f85
Stanislav B <bazarovstas@gmail.com>
2024-11-03 15:48:24 +0300

    add draft of the calc-game, refactoring game_engine.py

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -1,8 +1,17 @@
-# import random
-# import prompt
-# import brain_games.constants as const
-# from brain_games.game_engine import game_engine
+import random
+from brain_games.game_engine import game_engine
+from brain_games.constants import MATH_SYMBOLS
+from brain_games.constants import CALC_GAME_INTRODUCTION
+
+
+def get_expression_and_answer():
+    num1, num2 = random.randint(1, 21), random.randint(1, 11)
+    random_symbol = random.choice(MATH_SYMBOLS)
+
+    expression = f'{str(num1)} {random_symbol} {str(num2)}'
+    answer = str(eval(expression))
+    return expression, answer
 
 
 def calc_game():
-    pass
+    game_engine(get_expression_and_answer, CALC_GAME_INTRODUCTION)
