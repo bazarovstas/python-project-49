770404980eee73b20b2506440eb25be5e7590144
Stanislav B <bazarovstas@gmail.com>
2024-11-05 10:59:20 +0300

    reduced cognitive complexity of the main function calc.py

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -7,7 +7,6 @@ from brain_games.constants import CALC_GAME_INTRODUCTION
 def get_expression_and_answer():
     num1, num2 = random.randint(1, 21), random.randint(1, 11)
     random_symbol = random.choice(MATH_SYMBOLS)
-
     expression = f'{str(num1)} {random_symbol} {str(num2)}'
     answer = str(eval(expression))
     return expression, answer
