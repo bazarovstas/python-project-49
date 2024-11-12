beef5e6e374469ad08f3c9bd5725371ff612d07b
Stanislav B <bazarovstas@gmail.com>
2024-11-12 17:10:30 +0300

    fix autotest error in progression game

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -1,3 +1,4 @@
+from random import choice
 from brain_games.utils import get_random_number
 from brain_games.game_engine import game_engine
 from brain_games.constants import PROGRESSION_LENGTH
@@ -5,15 +6,16 @@ from brain_games.constants import PROG_GAME_BRIEFING
 
 
 def get_random_sequence_and_answer():
-    random_step = get_random_number(max_range=5)
+    random_step = get_random_number(min_range=2, max_range=5)
     start_number = get_random_number(max_range=15)
     sequence = []
     sequence.append(start_number)
     for i in range(PROGRESSION_LENGTH):
         sequence += [sequence[i] + random_step]
-    missed_num_index = get_random_number(min_range=0, max_range=len(sequence) - 1)
+    missed_num_index = choice(sequence)
     question = ''
     for i in range(len(sequence)):
+        print(sequence[i])
         question += ' ' + str(sequence[i]) if sequence[i] != missed_num_index else ' ..'
     return question.strip(), str(missed_num_index)
 
